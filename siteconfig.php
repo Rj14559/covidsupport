d81645ef90408018f20b94b30b908112534b8ff5
Rj14559 <83427901+Rj14559@users.noreply.github.com>
2021-05-28 00:53:21 +0530

    Add files via upload

---
+++ siteconfig.php
@@ -0,0 +1,108 @@
+<?php
+/* ------ MAIN SETTINGS ------ */
+/* Website URL (Without www!)(Do Not End With '/') */
+$site_url = 'https://yoursite.com';
+/* Website title */
+$site_title = 'Appinate';
+/* iTunes Store 2-Letter Country Code (http://appstore.armorthemes.com/countries) */
+$site_country = 'us';
+/* iTunes Affiliate Token (optional) */
+$itunes_id = '';
+/* AddThis ID for sharing analytics (optional) */
+$addthis_id = '';
+/* Disqus shortname for comments (optional) */
+$disqus_shortname = '';
+/* Contact form mail receiver (Optional) */
+$contact_email = 'Support@Samaxx.com';
+/* ------ SEO SETTINGS ------ */
+/* Homepage title */
+$homepage_title = 'Find Popular iOS Apps';
+/* Homepage meta description */
+$homepage_desc = 'Welcome to the best iOS App finder.';
+/* Website meta keywords */
+$site_keywords = 'AppStore, iOS apps, Popular Apps, Popular iOS Apps, Free iOS Apps, App of the day';
+/* Category page title (Comes between category title and site title) */
+$catpage_title = 'Apps';
+/* App single page title (Comes between App title and site title) */
+$app_page_title = 'App';
+/* ------ HOMEPAGE CAROUSEL CATEGORY (Get id from the category url) (Leave blank for all categories) ------ */
+$homeapps_genre_id = '';
+/* ------ HOMEPAGE FEATURED APPS (Get id from the app url) ------ */
+$featapp_id1 = '989330818';
+$featapp_id2 = '887947640';
+$featapp_id3 = '304878510';
+$featapp_id4 = '685014186';
+$featapp_id5 = '479516143';
+$featapp_id6 = '573116090';
+/* ------ TRANSLATION OPTIONS ------ */
+/* Homepage */
+$home_carousel_title = 'Popular Apps';
+$featapps_title = 'Featured Apps';
+$rmore_title = 'Read More';
+/* App Page */
+$byartist_mpage = 'By';
+$cat_mpage = 'Category';
+$release_mpage = 'Release Date';
+$curv_mpage = 'Current Version';
+$adrat_mpage = 'Adult Rating';
+$filsz_mpage = 'File Size';
+$dev_mpage = 'Developer';
+$comp_mpage = 'Compatibility';
+$req_mpage = 'Requires iOS';
+$req_end_mpage = 'or later.';
+$score_mpage = 'Score';
+$from_mpage = 'From';
+$ratings_mpage = 'Ratings';
+$itunelink_mpage = 'On itunes';
+$descriphead_mpage = 'Description';
+$scrhead_mpage = 'Screenshots';
+$reviewshead_mpage = 'Reviews';
+$reviewsby_mpage = 'By';
+$commentbox_mpage = 'Comments';
+$morebyartist_mpage = 'More by';
+/* Header Navigation */
+$home_title = 'Home';
+$newapps_title = 'New Apps';
+$newfreeapps_title = 'New Free Apps';
+$newpaidapps_title = 'New Paid Apps';
+$topapps_title = 'Top Apps';
+$topfreeapps_title = 'Top Free Apps';
+$topgrosapps_title = 'Top Grossing Apps';
+$toppaidapps_title = 'Top Paid Apps';
+$categories_title = 'Categories';
+/* Search Form */
+$searchf_text = 'Search for Apps...';
+/* Search Page */
+$searchresults_title = 'Search Results For';
+$search_noresults_title = 'Sorry, no results were found.';
+/* App Categories Titles */
+$gtitle_1 = 'Books';
+$gtitle_2 = 'Business';
+$gtitle_3 = 'Catalogs';
+$gtitle_4 = 'Education';
+$gtitle_5 = 'Entertainment';
+$gtitle_6 = 'Finance';
+$gtitle_7 = 'Food Drink';
+$gtitle_8 = 'Games';
+$gtitle_9 = 'Health Fitness';
+$gtitle_10 = 'Lifestyle';
+$gtitle_11 = 'Medical';
+$gtitle_12 = 'Music';
+$gtitle_13 = 'Navigation';
+$gtitle_14 = 'News';
+$gtitle_15 = 'Newsstand';
+$gtitle_16 = 'Photo Video';
+$gtitle_17 = 'Productivity';
+$gtitle_18 = 'Reference';
+$gtitle_19 = 'Shopping';
+$gtitle_20 = 'Social Networking';
+$gtitle_21 = 'Sports';
+$gtitle_22 = 'Travel';
+$gtitle_23 = 'Utilities';
+$gtitle_24 = 'Weather';
+/* Footer Links */
+$footnav_home = 'Home';
+$footnav_privacy = 'Privacy Policy';
+$footnav_dmca = 'DMCA';
+$footnav_contact = 'Contact';
+?>
